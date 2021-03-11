819e137175c62d3732e75bcbcdf021c4a1c365fd
Hitesh Sharma <hitesh.sharma_cs18@gla.ac.in>
2021-03-11 19:33:58 +0530

    function

--- src/Components/FunctionalComponent.js
+++ src/Components/FunctionalComponent.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 
 //to export single component
+
 export default function FunctionalComponent(props){
     return (
         <div>
+        <button onClick={()=>{alert("alert")}}>Alert</button>
         <h1>{props.title}</h1>
         <h1>{props.content}</h1>
         </div>
