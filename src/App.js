f3aaa33d1274b78280b85e2773379f59376b086a
Hitesh Sharma <hitesh.sharma_cs18@gla.ac.in>
2021-04-12 14:22:39 +0530

    Revert "Practical_10"
    This reverts commit 67ce69b00fd7ee82f650e094ce21435d4ffc7b9a.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import logo from './logo.svg';
 //import './App.css';
 import React from 'react';
-import Navbar from './Navbar'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import Home from './Practical/Home';
-import About from './Practical/About';
-import Contact from './Practical/Contact';
-import None from './Practical/p10_none';
+// if there is single component in FunctionalComponent then no need to specify exact component name and can write any name
+// import fcc from from './Components/FunctionalComponent';
+import FunctionalComponent from './Components/FunctionalComponent';
+import {Class_Component,Class_Component2} from './Components/ClassComponent';
 
+import LifeCycle from './LifeCycle/LifeCycle';
+import Test from './LifeCycle/Test_Unmount';
 
+import RS from './ReactState';
+
+import Counter from './Practical/Practical8_Counter';
+import More from './Practical/Practical8_b';
+
+import ReactHooks from './Components/ReactHooks'
+
+import Event from './events'
+
+import Table from './Practical/Practical_9';
 
 function App() {
   return (
-    <Router>
-      <Navbar/>
-      <Switch>
-        <Route exact path="/" component={Home}></Route>
-        <Route path="/about" component={About}></Route>
-        <Route path="/Contact" component={Contact}></Route>
-        <Route path="" component={None}></Route>
-      </Switch>
-    </Router>
+    <>
+    {/* <h1>Heading 1</h1>
+    <p>this is a para</p>
+    <FunctionalComponent title="Title_xyz__1" content="Conetent_XYZ__1"/>
+    <FunctionalComponent title="Title_xyz__2" content="Conetent_XYZ__2"/>
+    <Class_Component/>
+    <Class_Component2/> */}
+
+    {/* <LifeCycle/>
+    <Test/> */}
+
+    {/* <RS namee='hitesh' mob={5454545}/> */}
+
+    {/* <ReactHooks/> */}
+
+    {/* <Event/> */}
+
+    <Table/>
+
+
+    </>
   );
 }
 
+//Stateless Functional Component
+// function FunctionalComponent(){
+//   return <h1>StateLess</h1> 
+// }
+
+//Statefull
+// class ClassComponent extends React.Component{
+//   render(){
+//     return <p>StateFull</p>
+//   }
+// }
 
 export default App;
--- src/Practical/p10_none.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function None(){
-    return (
-        <>
-        <h1 class="ml-3">None</h1>
-        </>
-    );
-}
