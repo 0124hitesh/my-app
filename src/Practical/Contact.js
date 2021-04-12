67ce69b00fd7ee82f650e094ce21435d4ffc7b9a
Hitesh Sharma <hitesh.sharma_cs18@gla.ac.in>
2021-04-12 14:18:29 +0530

    Practical_10

---
+++ src/Practical/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Contact(){
+    return (
+        <>
+        <h1 class="ml-3">Contact</h1>
+        <p class="ml-3">lkjlrjwekrekfrjfkerje</p>
+        </>
+    );
+}
---
+++ src/Practical/p10_none.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function None(){
+    return (
+        <>
+        <h1 class="ml-3">None</h1>
+        </>
+    );
+}
