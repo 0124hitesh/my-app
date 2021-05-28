92d5c5fa003ba1b06b0a76cb87d1ff3896189ebd
0124hitesh <hitesh.sharma_cs18@gla.ac.in>
2021-05-28 17:27:04 +0530

    React-Express

--- src/React-Express.js
+++ src/React-Express.js
@@ -39,3 +39,17 @@ export default class React_Express extends Component{
         )
     }
 }
+
+
+// ################# Express Code
+// const express=require('express');
+// const app=express();
+
+// const port=process.env.PORT || 4000;
+// app.listen(port, ()=>{console.log("Server Connected " + port)});
+// const name="Express"
+
+// app.get('/express',(req,res)=>{
+//     res.send({name});
+// });
+// ###################
