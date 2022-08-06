384853bab63666e7ddd03972392e76a1823d1da8
Hitesh Sharma <44960249+0124hitesh@users.noreply.github.com>
2022-08-06 22:55:58 +0530

    useEffect

--- src/Components/ReactHooks.js
+++ src/Components/ReactHooks.js
@@ -1,20 +1,36 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function ReactHooks(){
+export default function ReactHooks() {
     // const [name]= useState("hitesh");
     const [name, setState_xyz] = useState("hitesh");
     const [mob] = useState(54545454);
 
-    useEffect(()=>{
+    var [a, setA] = useState(0);
+    var [b, setB] = useState(1);
+
+    function fun() {
+        setA(5);
+    }
+
+    function fun2() {
+        setB(2);
+    }
+
+    useEffect(() => {
         console.log("useEffect");
-    });
+    }, [a, b]);
+    // useEffect will recall after a or b value update
+    
     return (
         <>
-        <h1>{name}</h1>
-        <h2>{mob}</h2>
-        <button onClick={()=>{
-            setState_xyz("Hitesh Sharma");
-        }}>Click</button>
+            <h1>{name}</h1>
+            <h2>{mob}</h2>
+            <button onClick={() => {
+                setState_xyz("Hitesh Sharma");
+            }}>Click</button>
+            <br></br>
+            <button onClick={fun} >Hellllooooo</button>
+            <button onClick={fun2} >Hellllooo2</button>
         </>
     );
 }
